982b538441190982cab10ed75ae33d320eb080ad
Brian O'Donnell <bodonne3@depaul.edu>
2026-04-20 17:50:28 -0500

    Delete week3 directory

--- week3/src/pages/Contact.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-const Contact: React.FC = () => {
-
-    return(<div><h1>Contact</h1></div>)
-}
-export default Contact;
--- week3/src/pages/Settings.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Settings: React.FC = () => {
-  return <h2>Settings Page</h2>;
-};
-
-export default Settings;
